70ef837714d12134cfb90e5163b27f92b67cf114
Dries <rikmixer@gmail.com>
2018-06-13 13:46:02 +0200

    In dit bestand staan alle classifier imports van de classifiers die door Stefan gekozen zijn.

---
+++ classifiers.py
@@ -0,0 +1,4 @@
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
