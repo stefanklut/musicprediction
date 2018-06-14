d2d14d88cd0ebb51af7ca670a65b2578bc3f3df6
Jargogle22 <35296276+Jargogle22@users.noreply.github.com>
2018-06-14 12:43:55 +0200

    bug fixes

--- filter_music.py
+++ filter_music.py
@@ -5,7 +5,7 @@ Reads in names of erroneous files from a file named bad_files.txt append
 moves these files from the 'music' folder to an 'erroneous_music' folder.
 
 '''
-from os import listdir
+from os import listdir, mkdir
 import shutil
 
 # Create list of filenames.
@@ -17,7 +17,7 @@ with open('bad_files.txt', 'r') as f:
 f.close()
 
 # Move files to different folder.
-os.mkdir('erroneous_music')
+mkdir('erroneous_music')
 for filename in listdir('music'):
     if filename in file_list:
         shutil.move('music/'+filename, 'erroneous_music')
