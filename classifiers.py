7feea48a44ed3424badd300ea6221168b083a799
hannahnas <hannahmmin@gmail.com>
2018-06-19 10:46:14 +0200

    classifier with pca!

--- classifiers.py
+++ classifiers.py
@@ -2,18 +2,19 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
 
-def classify_features(func, feature_matrix, responses, test_size=0.30):
-    feature_matrix_train, feature_matrix_test, responses_train, \
-        responses_test = \
+def classify_features(func, feature_matrix, responses, test_size=0.30, pca=FALSE):
+    features_train, features_test, responses_train, responses_test = \
         train_test_split(feature_matrix, responses, test_size=test_size)
 
+    if pca:
+        features_train, features_test = apply_pca(features_train, features_test)
+
     classify_function = func()
-    classify_function.fit(feature_matrix_train, responses_train)
-    classify_prediction = classify_function.predict(feature_matrix_test)
+    classify_function.fit(features_train, responses_train)
+    classify_prediction = classify_function.predict(features_test)
     feature_importance = enumerate(classify_function.feature_importances_)
     return(*measures(*confusion_matrix(responses_test, classify_prediction).ravel()),
            sorted(feature_importance, key=lambda x:x[1])[-20:])
-
 
 def measures(tn, fp, fn, tp):
     accuracy = (tn + tp) / (tn + fp + fn + tp)
@@ -23,6 +24,21 @@ def measures(tn, fp, fn, tp):
     specificity = (tn) / (tn + fp)
 
     return (accuracy, precision, recall, f1_score, specificity)
+
+def apply_pca(feat_train, feat_test):
+    scaler = StandardScaler()
+    scaler.fit(feat_train)
+
+    feat_train = scaler.transform(feat_train)
+    feat_test = scaler.transform(feat_test)
+
+    pca = PCA(.95)
+    pca.fit(feat_train)
+
+    pca_train = pca.transform(feat_train)
+    pca_test = pca.transform(feat_train)
+
+    return pca_train, pca_test
 
 """
 svc = SVC()
