e257ba2f1a128e2795dab201c49447f84434e3a4
Dries <rikmixer@gmail.com>
2018-06-22 15:10:51 +0200

    rewrote file connector to work with song ids, normaal ben ik he

--- feat_resp_comb.py
+++ feat_resp_comb.py
@@ -6,27 +6,40 @@ A great discription of the file_connector
 '''
 import numpy as np
 
+# def feat_resp_comb(classification_type, response_data, participant_data_obj, noise=False,
+# 				 lower_lim=-0.1, upper_lim=0.1):
+# 	""" Creates feature data matrix and corresponding participant answers."""
 
-def feat_resp_comb(classification_type, response_data, participant_data_obj, noise=False,
-                 lower_lim=-0.1, upper_lim=0.1):
-    """ Creates feature data matrix and corresponding participant answers."""
+# 	responses = participant_data_obj.get(classification_type, 'is_response_correct')
+# 	song_id = participant_data_obj.get(classification_type, 'sound_cloud_id')
 
-    responses = participant_data_obj.get(classification_type, 'is_response_correct')
-    song_id = participant_data_obj.get(classification_type, 'sound_cloud_id')
+# 	feature_length = len(list(response_data.values())[0])
+# 	feature_data_union = list(set(song_id) & set(response_data.keys()))
 
-    feature_length = len(list(response_data.values())[0])
-    feature_data_union = list(set(song_id) & set(response_data.keys()))
+# 	feature_matrix = []
+# 	valid_song_id = []
 
-    feature_matrix = []
-    valid_song_id = []
+# 	for i, key in enumerate(song_id):
+# 		if key in feature_data_union:
+# 			feature_matrix.append(response_data[key])
+# 			valid_song_id.append(i)
 
-    for i, key in enumerate(song_id):
-        if key in feature_data_union:
-            feature_matrix.append(response_data[key])
-            valid_song_id.append(i)
+# 	feature_matrix = np.asarray(feature_matrix)
+# 	if noise:
+# 		feature_matrix += np.random.uniform(lower_lim, upper_lim, feature_matrix.shape)
 
-    feature_matrix = np.asarray(feature_matrix)
-    if noise:
-        feature_matrix += np.random.uniform(lower_lim, upper_lim, feature_matrix.shape)
+# 	return (feature_matrix, responses[valid_song_id])
 
-    return (feature_matrix, responses[valid_song_id])
+
+def create_folds(song_id_split, feat_dict, responses, class_type):
+    response_tf = responses.get(class_type, 'is_response_correct')
+    song_ids = responses.get(class_type, 'sound_cloud_id')
+    unique_song_ids = np.unique(song_ids)
+    id_location_dict = {sid:np.where(song_ids == sid)[0] for sid in unique_song_ids}
+    bucket_list = [[] for _ in range(len(song_id_split))]
+    for i, fold in enumerate(song_id_split):
+        for song_id in fold:
+            for response in id_location_dict[song_id]:
+                bucket_list[i].append(np.append(feat_dict[song_id], response_tf[response]))
+
+    return bucket_list
